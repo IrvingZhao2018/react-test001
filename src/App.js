ef56393eadcf0c74d50653080425d95a97106b17
irving <irvingzqy@gmail.com>
2020-09-17 18:15:03 -0400

    learn JSX

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg_back';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
             <h1>Hi, I'm a React App</h1>
+            <p> This works! </p>
         </div>
     );
+    // return React.createElement('div', null, 'h1', 'Hello World');
+    // return React.createElement('div', {className:'App'}, React.createElement('h1',null, 'Hello World'));
 }
 
 export default App;
