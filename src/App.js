ebc79ea870eb6ed9c25f307cb2f665e2799405d0
irving <irvingzqy@gmail.com>
2020-09-17 19:01:36 -0400

    Person component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
     return (
         <div className="App">
             <h1>Hi, I'm a React App</h1>
             <p> This works! </p>
+            <Person/>
         </div>
     );
     // return React.createElement('div', null, 'h1', 'Hello World');
---
+++ src/Person/Person.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// a component is just a function which returns some jsx, some html
+// ES6, function name started with lower case
+const person = () => {
+    return <p>I'm a Person!</p>
+};
+
+export default person;
+
+// function keyword directly
+// function person(){
+//     return <h2></h2>
+// }
+
+// ES5 syntax
+// var person = function (){
+//
+// }
